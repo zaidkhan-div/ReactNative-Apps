eb8b50ecc3728863866d19711150357827e655d5
zaidkhan-div <zaidscestudent@gmail.com>
2025-08-25 14:38:51 +0500

    so i am using json-server for my todos

--- Todo/Components/Welcome.jsx
+++ Todo/Components/Welcome.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Icon from "react-native-vector-icons/Entypo";
 import NewIcon from "react-native-vector-icons/Ionicons";
 import PerfectSize from "../utils/PerfectSize";
 import Theme from '../utils/theme.js'
 import { Link } from 'expo-router';
+import axios from "axios"
 
 const Welcome = () => {
     const [inputVal, setInputVal] = useState("");
-    console.log(inputVal);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await axios.get("http://192.168.0.119:3000/todos");
+                setData(response.data);
+            } catch (error) {
+                console.log(error.message);
+                setLoading(true)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData();
+    }, []);
 
     return (
         <>
@@ -41,6 +59,38 @@ const Welcome = () => {
                     </TouchableOpacity> */}
                 </View>
             </SafeAreaView>
+
+            <ScrollView style={styles.container}>
+                {loading ? (
+                    <Text style={styles.loading}>Loading...</Text>
+                ) : (
+                    data?.map((item, index) => (
+                        <View key={index} style={styles.todoCard}>
+                            {/* <CheckBox
+                                value={item?.completed}
+                                onValueChange={(value) => handleCheckbox(value, item)}
+                            /> */}
+                            <View style={styles.todoContent}>
+                                <Text style={styles.todoTitle}>{item?.title}</Text>
+                                <Text style={styles.todoDescription}>{item?.description}</Text>
+
+                                {/* <View style={styles.badgeContainer}>
+                                    <Text
+                                        style={[
+                                            styles.priorityBadge,
+                                            item?.priority === 'high' ? styles.priorityHigh : styles.priorityNormal
+                                        ]}
+                                    >
+                                        {item?.priority}
+                                    </Text>
+                                    <Text style={styles.badge}>Due: {item?.dueDate}</Text>
+                                    <Text style={styles.badge}>Estimated: {item?.estimatedHours}</Text>
+                                </View> */}
+                            </View>
+                        </View>
+                    ))
+                )}
+            </ScrollView>
         </>
     )
 }
@@ -91,5 +141,74 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: PerfectSize(20),
         cursor: "pointer"
-    }
+    },
+    // This is for the TodosContainer
+    container: {
+        // marginTop: 20,
+        flex: 1,
+    },
+    loading: {
+        textAlign: 'center',
+        fontSize: 20,
+        marginTop: 30,
+    },
+    todoCard: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 5,
+        elevation: 3,
+        paddingVertical: 15,
+        paddingHorizontal: 12,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        marginBottom: 15,
+        gap: 10,
+    },
+    todoContent: {
+        flex: 1,
+        flexDirection: 'column',
+        gap: 6,
+    },
+    todoTitle: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#000',
+    },
+    todoDescription: {
+        fontSize: 14,
+        color: '#6b7280',
+    },
+    badgeContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 10,
+        marginTop: 5,
+    },
+    badge: {
+        backgroundColor: '#f3f4f6',
+        color: '#6b7280',
+        fontSize: 12,
+        paddingVertical: 2,
+        paddingHorizontal: 8,
+        borderRadius: 15,
+        overflow: 'hidden',
+    },
+    priorityBadge: {
+        fontSize: 12,
+        paddingVertical: 2,
+        paddingHorizontal: 8,
+        borderRadius: 15,
+        overflow: 'hidden',
+    },
+    priorityHigh: {
+        backgroundColor: '#fee2e2',
+        color: '#ef4444',
+    },
+    priorityNormal: {
+        backgroundColor: '#f3f4f6',
+        color: '#6b7280',
+    },
 })
